ed28a5fb9b529c2b4b1d9a26b22ade78419fb81d
Matt Kellough <mrk10a@gmail.com>
2016-03-16 00:06:24 -0400

    Added Business controller and model

--- Gemfile
+++ Gemfile
@@ -20,6 +20,12 @@ gem 'jquery-rails'
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
+
+gem 'simple_form', '~> 3.2', '>= 3.2.1'
+
+gem 'paperclip', '~> 4.3', '>= 4.3.6'
+
+gem 'haml', '~> 4.0', '>= 4.0.7'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
@@ -44,4 +50,3 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
-
--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,4 +1,40 @@
 class BusinessesController < ApplicationController
   def index
   end
+
+  def show
+    @business = Business.find(params[:id])
+  end
+
+  def new
+    @business = Business.new
+  end
+
+  def create
+    @business = Business.new(business_params)
+
+    if @business.save
+      redirect_to @business
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_post
+  end
+
+  def business_params
+    params.require(:business).permit(:name, :description, :address, :city, :state, :zip)
+  end
 end
